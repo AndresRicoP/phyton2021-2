1a6eb3b1e5cf5b9770b0895d6c025e0666ea2727
AndrésRico <andres.ricoperez@institutoleones.mx>
2021-11-03 09:24:05 -0600

    últimos 2

---
+++ 5_potencia.py
@@ -0,0 +1,5 @@
+print("En este ejercicio se calculará la potencia Y de un número X")
+x = int(input("Da un número "))
+y = int(input("Da una potencia "))
+resultado = x**y
+print(f"El resultado de {x}^{y} = {resultado}")
---
+++ 8_area.py
@@ -0,0 +1,5 @@
+print("En este ejercicio se calculará y mostrará el área de un triángulo con los valores de base y altura")
+b= float(input("Da la base "))
+a= float(input("Da la altura "))
+resultado = (b*a)/2
+print(f"El área de un triángulo cuya base es {b} y altura es {a} es {resultado}")
