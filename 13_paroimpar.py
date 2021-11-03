86e5ae450b0547fd4e0cd885b871ebe95d6567ba
Andrés Rico <andres.ricoperez@institutoleones.mx>
2021-11-03 13:27:39 -0600

    Primero de IF

---
+++ 13_paroimpar.py
@@ -0,0 +1,6 @@
+print("El ejercicio muestra si un número es par o impar")
+n=int(input("Da un número "))
+if n%2==0:
+    print(f"El número {n} es par")
+else:
+    print(f"El número {n} es impar")
---
+++ 9_palabras.py
@@ -0,0 +1,7 @@
+print("En este ejercicio el programa determinará si dos palabras dadas son iguales o no")
+p1 = str(input("Da una palabra"))
+p2 = str(input("Da otra palabra"))
+if p1 == p2:
+    print("Las palabras son iguales")
+else:
+    print("Las palabras son diferentes")
